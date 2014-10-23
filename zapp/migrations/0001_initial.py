91beae3908392b67ac708a216a193a65d93e03e5
Jon Gjengset <jon@thesquareplanet.com>
2014-10-23 16:26:31 -0400

    Finalize models and add user's person

--- zapp/migrations/0001_initial.py
+++ zapp/migrations/0001_initial.py
@@ -8,17 +8,16 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Person',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.OneToOneField(serialize=False, primary_key=True, to=settings.AUTH_USER_MODEL)),
                 ('zoobars', models.IntegerField(default=10)),
                 ('profile', models.TextField(max_length=5000, default='')),
-                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
             },
@@ -27,11 +26,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Transfer',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('amount', models.IntegerField()),
-                ('time', models.DateTimeField()),
-                ('recipient', models.ForeignKey(related_name='received_transfers', to='zapp.Person')),
-                ('sender', models.ForeignKey(related_name='sent_transfers', to='zapp.Person')),
+                ('time', models.DateTimeField(auto_now=True)),
+                ('recipient', models.ForeignKey(to='zapp.Person', related_name='received_transfers')),
+                ('sender', models.ForeignKey(to='zapp.Person', related_name='sent_transfers')),
             ],
             options={
             },
--- zapp/models.py
+++ zapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Person(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, primary_key=True)
     zoobars = models.IntegerField(default=10)
     profile = models.TextField(max_length=5000, default="")
 
@@ -13,7 +13,18 @@ class Transfer(models.Model):
     sender = models.ForeignKey(Person, related_name='sent_transfers')
     recipient = models.ForeignKey(Person, related_name='received_transfers')
     amount = models.IntegerField()
-    time = models.DateTimeField()
+    time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "Transfer of %d zoobars from %s to %s" % (self.amount, self.sender.user.username, self.recipient.user.username)
+
+def create_person(instance, created, raw, **kwargs):
+    # Ignore fixtures and saves for existing courses.
+    if not created or raw:
+        return
+
+    person = Person()
+    person.user = instance
+    person.save()
+
+models.signals.post_save.connect(create_person, sender=User, dispatch_uid='create_user_person')
