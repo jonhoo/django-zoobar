94ecc55699d5099475e45b0662b6540d95b91b28
Jon Gjengset <jon@thesquareplanet.com>
2014-10-23 14:02:12 -0400

    Route zoobar requests to zapp

--- zapp/views.py
+++ zapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import zapp.models as z
 
-# Create your views here.
+def index(request):
+    pass
+
+def transfer(request):
+    pass
+
+def users(request):
+    pass
+
+def user(request, user_id):
+    pass
